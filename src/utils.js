0ea5e0577a8d9c235ddd7d1ecaca9d28e546cabf
Ricardo Tomasi <ricardobeat@gmail.com>
2012-11-22 03:06:42 -0200

    refactor

--- src/utils.js
+++ src/utils.js
@@ -1,41 +1,49 @@
 
-utils = {
-    encode: function(data, encoding) {
-        if (encoding === 'json'){
-            return JSON.stringify(data)
-        } else {
-            return data
-        }
-    }
-  , decode: function(data, encoding) {
-        if (encoding === 'json'){
-            return JSON.parse(data.toString())
-        } else {
-            return data
-        }
-    }
-  , extend: function(dest, src) {
-        for (var key in src){
-            if (Object.prototype.hasOwnProperty.call(parent, key)){
-                dest[key] = src[key]
-            }
-        }
-        return dest
-  }
-  , inherits = function(child, parent) {
-        util.extend(child, parent)
-        function ctor() {
-            this.constructor = child
-        }
-        ctor.prototype = parent.prototype
-        child.prototype = new ctor()
-        child.__super__ = parent.prototype
-        return child
-    }
-  , handleError = function(err, cb) {
-        if (cb) return cb(err)
-        throw err
+function encode (data, encoding) {
+    if (encoding === 'json'){
+        return JSON.stringify(data)
+    } else {
+        return data
     }
 }
 
-module.exports = utils
+function decode (data, encoding) {
+    if (encoding === 'json'){
+        return JSON.parse(data.toString())
+    } else {
+        return data
+    }
+}
+
+function extend (dest, src) {
+    for (var key in src){
+        if (Object.prototype.hasOwnProperty.call(parent, key)){
+            dest[key] = src[key]
+        }
+    }
+    return dest
+}
+
+function inherits (child, parent) {
+    extend(child, parent)
+    function ctor() {
+        this.constructor = child
+    }
+    ctor.prototype = parent.prototype
+    child.prototype = new ctor()
+    child.__super__ = parent.prototype
+    return child
+}
+
+function handleError (err, cb) {
+    if (cb) return cb(err)
+    throw err
+}
+
+module.exports = {
+    encode: encode
+  , decode: decode
+  , extend: extend
+  , inherits: inherits
+  , handleError: handleError
+}
